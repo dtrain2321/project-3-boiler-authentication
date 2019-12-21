b379a2173bb2c6104e8179dcb46f3f37ad63c487
Derek Stewart <derek.charles.stewart@gmail.com>
2019-12-21 11:25:10 -0500

    first push

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,32 @@
+// code to display nav bar
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Nav = (props) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link to="/">
+        <span className="nav-style navbar-brand">Cap Collector</span>
+      </Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <div className="navbar-nav">
+          <Link to="/home" style={{ margin: 5, textDecoration: "none" }}>
+            <span className="nav-style nav-item nav-link">Home</span>
+          </Link>
+          <Link to="/reviews" style={{ margin: 5, textDecoration: "none" }}>
+            <span className="nav-style nav-item nav-link">Reviews</span>
+          </Link>
+          <Link to="/beerList" style={{ margin: 5, textDecoration: "none" }}>
+            <span className="nav-style nav-item nav-link">BeerList</span>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
